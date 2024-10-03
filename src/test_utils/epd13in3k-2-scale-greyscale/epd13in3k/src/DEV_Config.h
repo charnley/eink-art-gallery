c7baa07c7de09df4c48d2aaf731599d2ff35474e
Peter Glerup Ericson <pg.ericson@gmail.com>
2024-10-03 21:03:42 +0200

    added feather v2 for 2 greyscale testing
    also moved the whole dir as arduino ide complained

--- src/test_utils/epd13in3k-2-scale-greyscale/epd13in3k/src/DEV_Config.h
+++ src/test_utils/epd13in3k-2-scale-greyscale/epd13in3k/src/DEV_Config.h
@@ -44,27 +44,8 @@
 /**
  * GPIO config
 **/
-// #define EPD_SCK_PIN  13 // yellow
-// #define EPD_MOSI_PIN 14 // blue
-// #define EPD_CS_PIN   15 // orange
-// #define EPD_RST_PIN  36 // white
-// #define EPD_DC_PIN   37 // green
-// #define EPD_BUSY_PIN 35 // purple
-// vcc/3.3 grey
-// gnd brown
-// #define PIN_SPI_SCK  13
-// #define PIN_SPI_DIN  14
-// #define PIN_SPI_CS   15
-// #define PIN_SPI_BUSY 35//19
-// #define PIN_SPI_RST  36//21
-// #define PIN_SPI_DC   37//22
-// #define EPD_SCK_PIN  13
-// #define EPD_MOSI_PIN 14
-// #define EPD_CS_PIN   15
-// #define EPD_RST_PIN  41
-// #define EPD_DC_PIN   42
-// #define EPD_BUSY_PIN 40
 
+// waveshare esp32 driver board - tested and works
 #define EPD_SCK_PIN  13
 #define EPD_MOSI_PIN 14
 #define EPD_CS_PIN   15
@@ -73,10 +54,19 @@
 #define EPD_BUSY_PIN 25
 // add the power to EN on the waveshare esp32 driver board
 
+// feather v2 - esp32 4MB flash and 2 MB PSRAM - tested and works
+#define EPD_SCK_PIN  13
+#define EPD_MOSI_PIN 14
+#define EPD_CS_PIN   15
+#define EPD_RST_PIN  12 // 26
+#define EPD_DC_PIN   27
+#define EPD_BUSY_PIN 21
+// add the power to EN on the esp32 feather v2
+
 #define GPIO_PIN_SET   1
 #define GPIO_PIN_RESET 0
 
-/**c:\code\E-Paper_ESP32_Driver_Board_Code\examples\esp32-waveshare-epd\examples\epd13in3k\src\EPD.h
+/**
  * GPIO read and write
 **/
 #define DEV_Digital_Write(_pin, _value) digitalWrite(_pin, _value == 0? LOW:HIGH)
