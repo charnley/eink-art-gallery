a8e4aab1793e05444f3d4fbb2287d9249ccac897
Jimmy Charnley Kromann <jimmy@charnley.dk>
2024-10-28 18:04:40 +0100

    Added palette atkinson

--- src/art_utils/__init__.py
+++ src/art_utils/__init__.py
@@ -31,8 +31,56 @@ def set_atkinson_dither_array(img: np.ndarray):
                     img[ny, nx] = img_yx
 
 
+def palette_to_thresholds(palette: np.ndarray):
+
+    # Calculate threshold array from palette
+    thresholds = np.arange(256, dtype=np.int32)
+    diff = np.abs(np.subtract.outer(palette, thresholds))
+    closest = diff.argmin(axis=0)
+    thresholds = palette[closest]
+    return thresholds
+
+
+@jit
+def set_atkinson_dither_array_palette(img: np.ndarray, thresholds: np.ndarray):
+    """changes img array with atkinson dithering"""
+
+    heigh = 255
+    low = 0
+
+    frac = 8  # Atkinson constant
+    neighbours = np.array([[1, 0], [2, 0], [-1, 1], [0, 1], [1, 1], [0, 2]])  # atkinson neighbours
+
+    height, width = img.shape
+    for y in range(height):
+        for x in range(width):
+            old = img[y, x]
+            old = np.min(np.array([old, 255]))
+
+            new = thresholds[old]  # look up palette conversion
+
+            err = (old - new) // frac
+            img[y, x] = new
+            for dx, dy in neighbours:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < width and 0 <= ny < height:
+                    # Make sure that img set is between 0 and 255 (negative error could surpass the value)
+                    img_yx = img[ny, nx] + err
+                    img_yx = np.minimum(heigh, np.maximum(img_yx, low))
+                    img[ny, nx] = img_yx
+
+
 def atkinson_dither(image: Image.Image) -> Image.Image:
     """ """
     img = np.array(image.convert("L"), dtype=np.int32)
     set_atkinson_dither_array(img)
+    return Image.fromarray(np.uint8(img))
+
+
+def atkinson_dither_with_palette(image: Image.Image, pallete: np.ndarray):
+
+    img = np.array(image.convert("L"), dtype=np.int32)
+    thresholds = palette_to_thresholds(pallete)
+    set_atkinson_dither_array_palette(img, thresholds)
+
     return Image.fromarray(np.uint8(img))
--- src/art_utils/network_utils.py
+++ src/art_utils/network_utils.py
@@ -8,7 +8,7 @@ def send_photo(image: Image.Image, url):
     """Send a photo to paper_api"""
 
     byte_io = BytesIO()
-    image.save(byte_io, "bmp")
+    image.save(byte_io, "png")
     byte_io.seek(0)
 
-    return requests.post(url=url, files=dict(file=("service.bmp", byte_io, "image/bmp")))
+    return requests.post(url=url, files=dict(file=("service.png", byte_io, "image/png")))
