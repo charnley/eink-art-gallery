a995b68e49b457d6bc9daefdf664734805efec28
Jimmy Charnley Kromann <jimmy@charnley.dk>
2024-11-17 22:24:50 +0100

    A lot of red support

--- src/art_service/__main__.py
+++ src/art_service/__main__.py
@@ -2,11 +2,13 @@ import logging
 import warnings
 
 from art_generator import load_flux_schnell, load_sd3, prompt_flux_schnell, prompt_sd3
-from art_utils import atkinson_dither
-from art_utils.network_utils import send_photo
+from art_utils import atkinson_dither, image_split_red_channel
+from art_utils.network_utils import send_photo, send_photo_red
 
 warnings.filterwarnings("ignore", category=UserWarning)
 logger = logging.getLogger(__name__)
+
+URL = "http://192.168.1.26:8080"
 
 
 def main(args=None):
@@ -19,6 +21,7 @@ def main(args=None):
 
     parser.add_argument("--prompt", type=str, default=None, nargs="+")
     parser.add_argument("--method", type=str, default="SD3", choices=["SD3", "FluxSchnell"])
+    parser.add_argument("--use-red", action="store_true", default=False)
 
     args = parser.parse_args(args)
 
@@ -33,6 +36,10 @@ def main(args=None):
     logger.info(f"prompt: {prompt}")
     logger.info("Generating a random photo")
 
+    if args.use_red:
+        logger.info("Add some red")
+        prompt = prompt.replace("bw", "bw and red")
+
     if args.method == "SD3":
         load_func = load_sd3
         prompt_func = prompt_sd3
@@ -40,6 +47,9 @@ def main(args=None):
     elif args.method == "FluxSchnell":
         load_func = load_flux_schnell
         prompt_func = prompt_flux_schnell
+
+    assert load_func is not None
+    assert prompt_func is not None
 
     # okay
     pipe = load_func()
@@ -49,12 +59,22 @@ def main(args=None):
     del pipe
 
     # Dither
-    logger.info("dithering the picture")
-    image = atkinson_dither(image)
 
-    logger.info("Sending it to paper frame")
-    send_photo(image, "http://192.168.1.26:8080/display/image")
+    if args.use_red:
+        logger.info("splitting and dithering the picture")
+        image_r, image_b = image_split_red_channel(image)
+        image_r = atkinson_dither(image_r)
+        image_b = atkinson_dither(image_b)
+        logger.info("Sending it to red paper frame")
+        r = send_photo_red(image_b, image_r, f"{URL}/display/redImage")
 
+    else:
+        logger.info("dithering the picture")
+        logger.info("Sending it to paper frame")
+        image = atkinson_dither(image)
+        r = send_photo(image, f"{URL}/display/image")
+
+    logger.info(f"Got {r} after {r.elapsed.total_seconds()} seconds")
     image.save("test.bmp")
 
 
--- src/art_utils/network_utils.py
+++ src/art_utils/network_utils.py
@@ -1,10 +1,10 @@
 from io import BytesIO
 
 import requests
-from PIL import Image
+from PIL.Image import Image
 
 
-def send_photo(image: Image.Image, url):
+def send_photo(image: Image, url):
     """Send a photo to paper_api"""
 
     byte_io = BytesIO()
@@ -12,3 +12,23 @@ def send_photo(image: Image.Image, url):
     byte_io.seek(0)
 
     return requests.post(url=url, files=dict(file=("service.png", byte_io, "image/png")))
+
+
+def send_photo_red(image_red: Image, image_black: Image, url):
+    """Send a photo to paper_api"""
+
+    byte_io_red = BytesIO()
+    image_red.save(byte_io_red, "png")
+    byte_io_red.seek(0)
+
+    byte_io_black = BytesIO()
+    image_black.save(byte_io_black, "png")
+    byte_io_black.seek(0)
+
+    return requests.post(
+        url=url,
+        files=dict(
+            redFile=("red.png", byte_io_red, "image/png"),
+            blackFile=("black.png", byte_io_black, "image/png"),
+        ),
+    )
