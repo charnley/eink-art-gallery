75a96c5c08d9bb053233dad61d45a13fffec5820
Jimmy Charnley Kromann <jimmy@charnley.dk>
2025-01-31 21:12:58 +0100

    Iteration

--- services/ha_addon_canvasserver/src/canvasserver/routes/__init__.py
+++ services/ha_addon_canvasserver/src/canvasserver/routes/__init__.py
@@ -1,6 +1,10 @@
 from fastapi import APIRouter
 
-from .images import router
+from .actions import router as action_router
+from .images import router as image_router
+from .prompts import router as prompt_router
 
 api_router = APIRouter()
-api_router.include_router(router)
+api_router.include_router(image_router)
+api_router.include_router(prompt_router)
+api_router.include_router(action_router)
