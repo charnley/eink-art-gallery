3075cecb2c314fec6a3f9b21340117549d66d5eb
Jimmy Charnley Kromann <jimmy@charnley.dk>
2025-03-09 15:12:16 +0100

    Added dithering utils

---
+++ services/shared/src/shared_image_utils/__init__.py
@@ -0,0 +1,21 @@
+
+from shared_constants import FILE_UPLOAD_KEY, IMAGE_CONTENT_TYPE, IMAGE_FORMAT
+
+from PIL import Image
+from io import BytesIO
+
+
+def image_to_bytes(image: Image.Image):
+    """Return bytes of image in default image format"""
+
+    with BytesIO() as buf:
+        image.save(buf, format=IMAGE_FORMAT)
+        image_bytes = buf.getvalue()
+
+    return image_bytes
+
+
+def bytes_to_image(image_data) -> Image.Image:
+    image = Image.open(BytesIO(image_data))
+    return image
+
