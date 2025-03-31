7a3a99f8a294da74c875ea1a5e87aa43546f6c2e
Jimmy Charnley Kromann <jimmy@charnley.dk>
2025-03-31 17:09:03 +0200

    Added the file I forgot

---
+++ services/canvas_coordinator/src/canvasserver/jobs/__init__.py
@@ -0,0 +1,21 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def refresh_active_prompt():
+
+    # TODO Which theme is active?
+
+    # TODO Find a random prompt with more than six images
+
+    # TODO if not, find a prompt with any images
+
+    logger.info("HOLA!")
+
+    return
+
+
+def send_images_to_push_devices():
+
+    return
