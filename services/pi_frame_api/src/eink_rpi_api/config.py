ee16381cb08830ed9897494e1c82ebcef17bb005
Jimmy Charnley Kromann <jimmy@charnley.dk>
2025-07-04 16:18:21 +0200

    Fixed epdtype on pi

--- services/pi_frame_api/src/eink_rpi_api/config.py
+++ services/pi_frame_api/src/eink_rpi_api/config.py
@@ -6,9 +6,9 @@ from pathlib import Path
 from typing import Optional
 
 from pydantic_settings import BaseSettings
+from shared_constants import WaveshareDisplay
 
 from .constants import ENV_CONFIG_PATH
-from .displaying import EpdType
 
 logger = logging.getLogger(__name__)
 
@@ -16,7 +16,7 @@ logger = logging.getLogger(__name__)
 class Settings(BaseSettings):
     log_level: str = "INFO"
     cron_refresh: Optional[str] = None
-    EPD_TYPE: EpdType
+    EPD_TYPE: WaveshareDisplay
 
 
 @lru_cache
@@ -34,15 +34,18 @@ def get_settings(config_path: Optional[Path] = None):
         with open(config_path, "r") as f:
             options = json.load(f)
 
-    logger.info(f"reading options: {options}")
+    if options:
+        logger.info(f"reading options: {options}")
 
-    # Read and set EPD_TYPE constant
-    epd_type = EpdType.from_label(options["EPD_TYPE"])
-    EPD_TYPE = EpdType(epd_type)
-    assert isinstance(EPD_TYPE, EpdType)
+        # Read and set EPD_TYPE constant
+        epd_type = WaveshareDisplay(options["EPD_TYPE"])
+        assert isinstance(EPD_TYPE, WaveshareDisplay)
 
-    options["EPD_TYPE"] = epd_type
-    settings = Settings(**options)
+        options["EPD_TYPE"] = epd_type
+        settings = Settings(**options)
+
+    else:
+        raise ValueError("Could not find any options to read")
 
     logger.info(f"config: {settings}")
     return settings
