2b1e8b26fb18f1e8ba8443299f68893d2e3b49de
Jimmy Charnley Kromann <jimmy@charnley.dk>
2025-10-29 17:31:30 +0100

    More fonts

---
+++ services/shared/scripts/install_cormorant.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+set -u
+
+mkdir -p ~/.fonts
+mkdir -p tmp_fonts
+
+wget -O tmp_fonts/fonts.zip https://github.com/CatharsisFonts/Cormorant/releases/download/v4.002/Cormorant_Essentials_v4.002.zip
+cd tmp_fonts && unzip fonts.zip
+find tmp_fonts -type f -iname "*.otf" -exec cp {} ~/.fonts/ \;
+
+rm -r tmp_fonts
--- services/shared/scripts/install_nanummyeongjo.sh
+++ services/shared/scripts/install_nanummyeongjo.sh
@@ -4,13 +4,13 @@ set -x
 set -e
 set -u
 
+mkdir -p ${HOME}/.fonts/
 mkdir -p tmp_fonts
 
 wget -O tmp_fonts/NanumMyeongjo-Bold.ttf https://github.com/google/fonts/raw/refs/heads/main/ofl/nanummyeongjo/NanumMyeongjo-Bold.ttf
 wget -O tmp_fonts/NanumMyeongjo-Regular.ttf https://github.com/google/fonts/raw/refs/heads/main/ofl/nanummyeongjo/NanumMyeongjo-Regular.ttf
 wget -O tmp_fonts/NanumMyeongjo-ExtraBold.ttf https://github.com/google/fonts/raw/refs/heads/main/ofl/nanummyeongjo/NanumMyeongjo-ExtraBold.ttf
 
-mkdir -p ${HOME}/.fonts/
 cp tmp_fonts/*.ttf ${HOME}/.fonts/
 
 rm -r tmp_fonts
