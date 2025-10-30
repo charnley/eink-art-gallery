bbf5476949e70812a0ace544c0e2a49dd938e83c
Jimmy Charnley Kromann <jimmy@charnley.dk>
2025-10-30 22:47:12 +0100

    What a pain to get the docker image to work again

--- services/shared/scripts/install_cormorant.sh
+++ services/shared/scripts/install_cormorant.sh
@@ -9,6 +9,7 @@ mkdir -p tmp_fonts
 
 wget -O tmp_fonts/fonts.zip https://github.com/CatharsisFonts/Cormorant/releases/download/v4.002/Cormorant_Essentials_v4.002.zip
 cd tmp_fonts && unzip fonts.zip
+cd ..
 find tmp_fonts -type f -iname "*.otf" -exec cp {} ~/.fonts/ \;
 
 rm -r tmp_fonts
